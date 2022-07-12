530714174a56b88cc06ad08daee0be891d3da9f0
Zephelion <russellnumo@outlook.com>
2022-07-12 21:55:13 +0200

    searchcontainer component

---
+++ src/components/SearchContainer.js
@@ -0,0 +1,13 @@
+import SearchBar from "./SearchBar"
+
+const Search = () => {
+  return (
+	<div className="min-h-screen bg-white flex justify-center items-center">
+		<div className="container mx-auto bg-white rounded-lg p-14 drop-shadow-lg">
+			<SearchBar />
+		</div>
+	</div>
+  )
+}
+
+export default Search
