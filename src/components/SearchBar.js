ea4a96d589f4c232e302c249637bc625ccb9fd3d
Zephelion <russellnumo@outlook.com>
2022-07-12 21:59:49 +0200

    cleanup

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,13 +5,8 @@ const SearchBar = () => {
     const [searchTerm, setSearchTerm] = useState("");
     const [searchResults, setSearchResults] = useState([]);
 
-    const test = (e) => {
-        e.preventDefault();
 
-        console.log(searchTerm);
-    }
-
-    const testApi = async (e) => {
+    const fetchMusic = async (e) => {
         e.preventDefault();
         const api = await fetch(`http://api.musixmatch.com/ws/1.1/track.search?q_track=${searchTerm}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_API_KEY}`);
 
@@ -23,7 +18,7 @@ const SearchBar = () => {
 
   return (
 
-    <form onSubmit={testApi}>
+    <form onSubmit={fetchMusic}>
         <h1 className="text-center font-light text-black text-4xl">Music Search</h1>
             <p className="mx-auto font-normal text-sm my-6 max-w-lg text-center">Look for your favorite song based on the lyrics you have heard</p>
             <div className="sm:flex items-center bg-white rounded-lg overflow-hidden px-2 py-1 justify-between">
