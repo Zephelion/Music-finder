6590a1d126f4ad0a5d6871643f0f679921624a9f
Zephelion <russellnumo@outlook.com>
2022-07-25 22:06:01 +0200

    changed order

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,17 +1,11 @@
 import SearchBar from "./SearchBar"
 
-const SearchContainer = () => {
-
-	const pull_data = (data) => {
-		console.log(data);
-	}
+const SearchContainer = ({func}) => {
 
   return (
 
 		<div className="container mx-auto bg-white rounded-lg p-14 drop-shadow-lg border-solid border-2 border-gray-100">
-			<SearchBar
-				func = {pull_data}
-			/>
+			<SearchBar songs={func}/>
 		</div>
   )
 }
