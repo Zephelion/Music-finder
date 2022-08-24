c1bd4eedbac423cfe7d0779b847d16b15048a811
Zephelion <russellnumo@outlook.com>
2022-08-24 02:35:58 +0200

    get lyrics

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,8 +1,27 @@
-const Song = ({song}) => {
+import axios from "axios";
+import {useState} from 'react';
+
+const Song = ({song, lyric}) => {
+
+  const [lyrics, setLyrics] = useState("");
+
+  const handleClick = () => {
+    const url = `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${lyric}&apikey=${process.env.REACT_APP_API_KEY}`;
+    axios({
+      url: url,
+      method:"GET",
+    })
+    .then(res => {
+      console.log(res.data.message.body.lyrics.lyrics_body);
+      console.log(lyrics);
+      setLyrics(res.data.message.body.lyrics.lyrics_body);
+    })
+
+  }
+
   return (
-    <div className="container bg-white drop-shadow-sm rounded-md p-4 flex mb-4 border-solid border-2 border-gray-100 h-32">
+    <div onClick={handleClick} className="container bg-white drop-shadow-sm rounded-md p-4 flex mb-4 border-solid border-2 border-gray-100 h-32">
       <div className="bg-indigo-300 w-28 rounded-md mr-4">
-        {/* <img src={song.track.album_coverart_350x350} alt="album cover" /> */}
       </div>
       <div>
         <p className="text-lg font-semibold">{song.track.track_name} - {song.track.artist_name}</p>
--- src/components/Songs.js
+++ src/components/Songs.js
@@ -1,12 +1,13 @@
 import Song from './Song'
 
 const Songs = ({songs}) => {
+
   return (
     <>
       <div className="container mx-auto">
 
         {songs.map((song) => (
-          <Song key={song.track.track_id} song={song} />
+          <Song key={song.track.track_id} song={song} lyric={song.track.track_id} />
         ))}
       </div>
     
